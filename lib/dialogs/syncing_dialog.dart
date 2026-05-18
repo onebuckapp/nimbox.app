bb9ee87faf96e9bcb0b2cebb1c1f91d20fc2760d
George Lemon <georgelemon@protonmail.com>
2026-05-18 14:56:39 +0300

    update gpl notice
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- lib/dialogs/syncing_dialog.dart
+++ lib/dialogs/syncing_dialog.dart
@@ -1,7 +1,21 @@
-// Nimbox - The missing GUI for Nimble, Nim's package manager.
-//      Copyright (c) 2026 George Lemon
-//      Released under the GPLv3 License
-//      https://onebuck.app | https://github.com/onebuckapp
+/*
+    Nimbox - The missing GUI for Nimble, Nim's package manager.
+
+    Copyright (C) 2026  George Lemon from OpenPeeps
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 import 'dart:async';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
